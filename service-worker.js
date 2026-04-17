3c05cd13b2226b358a77d77b2e3c86d89bffbbad
Guido Rossitto <guido.rossitto@gmail.com>
2026-04-16 21:13:24 -0300

    link ig

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = "agendapp-shell-v59";
-const RUNTIME_CACHE = "agendapp-runtime-v59";
+const CACHE_NAME = "agendapp-shell-v60";
+const RUNTIME_CACHE = "agendapp-runtime-v60";
 
 const APP_SHELL = [
   "/",
