12cc7ae22424bf0873baa9155bfd905d351dec69
Guido Rossitto <guido.rossitto@gmail.com>
2026-04-18 18:54:00 -0300

    mejorando columnas y filas de lugar en movil

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = "agendapp-shell-v65";
-const RUNTIME_CACHE = "agendapp-runtime-v65";
+const CACHE_NAME = "agendapp-shell-v66";
+const RUNTIME_CACHE = "agendapp-runtime-v66";
 
 const APP_SHELL = [
   "/",
