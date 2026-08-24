5a6dffa0755cd67de101835fd6fbf9236cc81c5d
Guido Rossitto <guido.rossitto@gmail.com>
2026-08-24 13:36:12 -0300

    activando modo tango BA2

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = "agendapp-shell-v100";
-const RUNTIME_CACHE = "agendapp-runtime-v100";
+const CACHE_NAME = "agendapp-shell-v101";
+const RUNTIME_CACHE = "agendapp-runtime-v101";
 
 const APP_SHELL = [
   "/",
